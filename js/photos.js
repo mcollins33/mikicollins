8bfe10e0bf20946343e20a5a13357e83b1371370
mcollins33 <collins.miki@gmail.com>
2018-03-19 16:25:46 -0400

    add copyright

--- js/photos.js
+++ js/photos.js
@@ -52,6 +52,7 @@ $(document).ready(function() {
         $(".buttons").append($("<a href='./hobbies.html'><button type='button' class='waves-effect waves-light btn-large links' id='hobbies'>Hobbies</button></a>").hide().fadeIn(500));
         $(".buttons").append($("<a class='btn-floating btn-large links' href='https://www.linkedin.com/in/miki-collins/''><img class='icon' src='./images/linkedin.png' alt='Linked In'></a>").hide().fadeIn(500));
         $(".buttons").append($("<a class='btn-floating btn-large links' href='https://github.com/mcollins33'><img class='icon' src='./images/github.png' alt='GitHub'></a>").hide().fadeIn(500));
+        $("#footer-title").html("&copy 2018 Miki Collins");
     }, 1500);
 
 });
